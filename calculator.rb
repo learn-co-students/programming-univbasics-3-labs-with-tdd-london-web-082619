f102a44fd507d788101fdf44effdacdc63343a3c
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-11 23:21:59 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = "Hello World!"
+expect(second_number).not_to equal(0)
+sum = first_number + second_number
+difference = first_number-second_number
+product = first_number * second_number
+quotient = first_number/second_number
+
